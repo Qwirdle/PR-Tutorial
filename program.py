36b689050d7dbcc543dbdf602144902561e92e31
QuirkyGuy <129766022+QuirkyGuy@users.noreply.github.com>
2023-12-20 10:55:55 -0500

    fix program.py

--- program.py
+++ program.py
@@ -1,10 +1,10 @@
 # i think i messed up in a couple lines, can yall fix it for me?
+#fixed.
+favoriteAnimal = input("What's your favorite animal?")
 
-favoriteAnimal = innnput("What's your favorite animal?")
+if favoriteAnimal.lower != "cat":
+    print("wow so you hate cats?")
+    print("that's not very nice, bro.")
 
-if favoriteanimal.lower != "cat":
-    print("wow so you hate cats?"
-    print("that's not very nice", bro.)
-
-elsa:
-    qrint("I also love cats!")
+else:
+    print("I also love cats!")
